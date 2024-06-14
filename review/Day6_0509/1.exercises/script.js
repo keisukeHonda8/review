89b221cd4e24f70e59dfa26a472988470b93babd
keisukeHonda8 <kceans-eleven@hotmail.co.jp>
2024-06-15 01:59:31 +0900

    day6 succession

--- review/Day6_0509/1.exercises/script.js
+++ review/Day6_0509/1.exercises/script.js
@@ -4,33 +4,23 @@
 /* from here */
 /* テスト駆動開発（Test-driven Development [TDD]） Warmup */
 /* 1. 関数: compareTwoNumbers を宣言してください。*/
+//JSDoc
 /**
- * @param {number} ??? - 1 番目の数字
- * @param {number} ??? - 2 番目の数字
+ * @param {number} number1 - 1 番目の数字
+ * @param {number} number2 - 2 番目の数字
  * @returns {"X は Y と等しい" | "X は Y より小さい" | "X は Y より大きい"} 与えられた 2 つの数字の比較結果
  */
-// ここに関数を定義しましょう
-
-let actual = compareTwoNumbers(1, 1);
-let expected = "1 は 1 と等しい";
-
-if (actual === expected) {
-  console.log("OK! Test PASSED.");
-} else {
-  console.error("Test FAILED. Try again!");
-  console.log("    actual: ", actual);
-  console.log("  expected: ", expected);
+//func
+function compareTwoNumbers(number1, number2) {
+  if (number1 === number2) {
+    return `${number1} は ${number2} と等しい`;
+  } else if (number1 < number2) {
+    return `${number1} は ${number2} より小さい`;
+  } else if (number1 > number2) {
+    return `${number1} は ${number2} より大きい`;
+  }
 }
-
-actual = compareTwoNumbers(3, 4);
-expected = "3 は 4 より小さい";
-
-if (actual === expected) {
-  console.log("OK! Test PASSED.");
-} else {
-  console.error("Test FAILED. Try again!");
-  console.log("    actual: ", actual);
-  console.log("  expected: ", expected);
-}
-
-// 期待値が "X は Y より大きい" になるテストを書いてみましょう
+//test
+test(compareTwoNumbers(1, 1), "1 は 1 と等しい");
+test(compareTwoNumbers(3, 4), "3 は 4 より小さい");
+test(compareTwoNumbers(8, 4), "8 は 4 より大きい");
